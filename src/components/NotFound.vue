76ff16812ab903f0f8a010362a30dc80aaa53a27
Kieran <me@kieranajp.co.uk>
2017-04-12 10:56:19 +0200

    Switch to history router and add NotFound page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>404</h1>
+
+    <p>The page {{ url }} was not found</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: this.$state.route.fullPath
+    };
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import ApiList from '@/components/ApiList';
+import NotFound from '@/components/NotFound';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'ApiList',
       component: ApiList,
     },
+    {
+      path: '*',
+      name: '404',
+      component: NotFound
+    }
   ],
 });
