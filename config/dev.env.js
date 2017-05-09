b86b727ccaaeae51e9565b5c5f93e47514cbfa54
Kieran <me@kieranajp.co.uk>
2017-05-09 20:41:59 +0200

    Fix variable interpolation in env

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,8 +5,8 @@ var config = {
   NODE_ENV: '"development"',
   USE_MOCKS: process.env.USE_MOCKS || false,
   GATEWAY_BASE_URI: process.env.GATEWAY_BASE_URI ? `"${process.env.GATEWAY_BASE_URI}"` : '"http://localhost:8081"',
-  GATEWAY_USERNAME: process.env.GATEWAY_USERNAME ? `${process.env.GATEWAY_USERNAME}` : '"admin"',
-  GATEWAY_PASSWORD: process.env.GATEWAY_PASSWORD ? `${process.env.GATEWAY_PASSWORD}` : '"admin"',
+  GATEWAY_USERNAME: process.env.GATEWAY_USERNAME ? `"${process.env.GATEWAY_USERNAME}"` : '"admin"',
+  GATEWAY_PASSWORD: process.env.GATEWAY_PASSWORD ? `"${process.env.GATEWAY_PASSWORD}"` : '"admin"',
 };
 
 console.log(config);
--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export default {
   gateway: {
     uri: `${process.env.GATEWAY_BASE_URI}`,
     username: `${process.env.GATEWAY_USERNAME}`,
-    password: `${process.env.GATEWAY_PASSWORD}`,
+    password: `"${process.env.GATEWAY_PASSWORD}"`,
   },
 
 };
