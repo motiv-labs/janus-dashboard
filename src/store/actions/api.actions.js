7204b604fbd6e01a8b082c89c71192c14b59ffac
ivanzusko <ivan.zusko@gmail.com>
2017-07-14 11:33:07 +0200

    add basic error handling

--- src/store/actions/api.actions.js
+++ src/store/actions/api.actions.js
@@ -74,8 +74,23 @@ export const updateAPI = (pathname, api) => dispatch => {
     .then((response) => {
       dispatch(saveAPISuccess(JSON.parse(response.config.data)));
     })
-    .catch((e) => {
-      console.log('SAVE_API_ERROR', 'Infernal server error', e);
+    .catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        // More info about error handling in Axios: https://github.com/mzabriskie/axios#handling-errors
+        console.error(error.response.data);
+      } 
+      else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      }
+      else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
     });
 };
 
@@ -86,7 +101,23 @@ export const saveAPI = (pathname, api) => dispatch => {
     .then((response) => {
       dispatch(saveAPISuccess(JSON.parse(response.config.data)));
     })
-    .catch((e) => {
-      console.log('SAVE_API_ERROR', 'Infernal server error', e);
+    .catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        // More info about error handling in Axios: https://github.com/mzabriskie/axios#handling-errors
+        console.error(error.response.data);
+      } 
+      else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      }
+      else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
     });
+
 };
