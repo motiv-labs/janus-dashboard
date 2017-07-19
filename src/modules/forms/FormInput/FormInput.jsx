7abf0db4b28443ab1fa12f1c940c736ee9cf8f71
ivanzusko <ivan.zusko@gmail.com>
2017-07-19 14:15:38 +0200

    add styles and icon for search bar and remove some redundant stuff

---
+++ src/modules/forms/FormInput/FormInput.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Field } from 'redux-form';
+
+import FormField from '../FormField';
+import FormLabel from '../FormLabel';
+
+import './FormInput.css';
+
+const propTypes = {
+  attachTo: PropTypes.string.isRequired,
+  component: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  normalize: PropTypes.func,
+  tooltip: PropTypes.string,
+};
+
+const renderField = (component, type, name, normalize, parse, disabled) => {
+  const props = {
+    name,
+    component,
+    type,
+    normalize,
+    parse,
+    disabled,
+  };
+  return (
+    <Field className="j-input" {...props} />
+  );
+};
+
+const FormInput = (props) => {
+  const { attachTo, component, type, tooltip, normalize, parse, disabled } = props;
+
+  return (
+    <FormField>
+      <FormLabel htmlFor={attachTo} text={props.label} tooltip={tooltip} />
+      {renderField(component, type, attachTo, normalize, parse, disabled)}
+    </FormField>
+  );
+};
+
+FormInput.propTypes = propTypes;
+
+export default FormInput;
