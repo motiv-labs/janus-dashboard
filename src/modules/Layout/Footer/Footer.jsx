f9e84a0e88789c82ea7a51ad767372b8ab5c5fe0
ivanzusko <ivan.zusko@gmail.com>
2017-07-21 11:56:05 +0200

    footer styles and markup

--- src/modules/Layout/Footer/Footer.jsx
+++ src/modules/Layout/Footer/Footer.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
+import block from '../../../helpers/bem-cn';
+
+import './Footer.css';
+
+const b = block('j-footer');
 
 const Footer = () => {
   return (
-    <footer className="j-footer">
-      <div className="container">
-        <div className="content has-text-centered">
-          <p>
-            <strong>Janus Dashboard</strong> by <a href="http://hellofresh.com">HelloFresh</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-          </p>
-          <p>
-            <a href="https://github.com/hellofresh/janus-dashboard" target="_blank" rel="noopener noreferrer" className="j-nav-item source-code-icon">
-              <span className="icon">&#60; &#62;</span>
-            </a>
-          </p>
-        </div>
-      </div>
+    <footer className={b}>
+      <p>
+        <strong>Janus</strong> by <a href="http://hellofresh.com">HelloFresh</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+      </p>
     </footer>
   );
 };
