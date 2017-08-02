11e915454d4f6fdf818ba062fdcbf97f32c81141
ivanzusko <ivan.zusko@gmail.com>
2017-08-02 16:02:38 +0200

    basic styles for new api form

--- src/modules/pages/NewApiPage/NewApiForm.js
+++ src/modules/pages/NewApiPage/NewApiForm.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 
+import block from '../../../helpers/bem-cn';
+
 import Section from '../../Layout/Section/Section';
+import Title from '../../Layout/Title/Title';
 import FormRow from '../../forms/FormRow';
 import FormInput from '../../forms/FormInput/FormInput';
 import FormLabel from '../../forms/FormLabel';
 import Button from '../../buttons/Button';
 import RenderPlugins from '../../forms/RenderPlugins';
+
+import './NewApiForm.css';
+
+const b = block('j-api-form');
 
 const propTypes = {
     handleSubmit: PropTypes.func.isRequired,
@@ -25,42 +32,44 @@ const ApiForm = (props) => {
     return (
         <form onSubmit={handleSubmit}>
             <Section>
-                <FormRow>
-                    <FormInput component="input" label="Name" attachTo="name" type="text" tooltip="some another tooltip about something usefull" />
+                <Title>Create New API</Title>
+                <FormRow alignX>
+                    <Button
+                        type="submit"
+                        mod="primary"
+                    >
+                        Submit
+                    </Button>
                 </FormRow>
-            </Section>
-            <Section>
-                <FormRow>
-                    <FormInput component="input" label="Listen Path" attachTo="proxy.listen_path" type="text" tooltip="LOREM IPSUM" />
-                    <FormInput component="input" label="Upstream URL" attachTo="proxy.upstream_url" type="text" />
-                    <FormInput component="input" label="Preserve HOST" attachTo="proxy.preserve_host" type="checkbox" normalize={v => !!v} />
-                    <FormInput component="input" label="Strip Path" attachTo="proxy.strip_path" type="checkbox" normalize={v => !!v} />
-                    <FormInput component="input" label="Append Path" attachTo="proxy.append_path" type="checkbox" normalize={v => !!v} />
-                </FormRow>
-                <Section>
+                <div className={b('inner')}>
                     <FormRow>
-                        <FormLabel text="Health check" />
+                        <FormInput component="input" label="Name" attachTo="name" type="text" tooltip="some another tooltip about something usefull" />
                     </FormRow>
                     <FormRow>
-                        <FormInput component="input" label="url" attachTo="health_check.url" type="text" />
-                        <FormInput component="input" label="timeout" attachTo="health_check.timeout" type="text" parse={parse} />
+                        <FormInput component="input" label="Listen Path" attachTo="proxy.listen_path" type="text" tooltip="LOREM IPSUM" />
+                        <FormInput component="input" label="Upstream URL" attachTo="proxy.upstream_url" type="text" />
+                        <FormInput component="input" label="Preserve HOST" attachTo="proxy.preserve_host" type="checkbox" normalize={v => !!v} />
+                        <FormInput component="input" label="Strip Path" attachTo="proxy.strip_path" type="checkbox" normalize={v => !!v} />
+                        <FormInput component="input" label="Append Path" attachTo="proxy.append_path" type="checkbox" normalize={v => !!v} />
                     </FormRow>
-                </Section>
+                    <Section>
+                        <FormRow>
+                            <FormLabel text="Health check" />
+                        </FormRow>
+                        <FormRow>
+                            <FormInput component="input" label="url" attachTo="health_check.url" type="text" />
+                            <FormInput component="input" label="timeout" attachTo="health_check.timeout" type="text" parse={parse} />
+                        </FormRow>
+                    </Section>
+
+
+                    {
+                        !!initialValues.plugins &&
+                            <RenderPlugins plugins={initialValues.plugins} />
+                    }
+
+                </div>
             </Section>
-
-            {
-                !!initialValues.plugins &&
-                    <RenderPlugins plugins={initialValues.plugins} />
-            }
-
-            <FormRow alignX>
-                <Button
-                    type="submit"
-                    mod="primary"
-                >
-                    Submit
-                </Button>
-            </FormRow>
         </form>
     );
 };
--- src/modules/pages/NewApiPage/NewApiPage.js
+++ src/modules/pages/NewApiPage/NewApiPage.js
@@ -3,23 +3,11 @@ import PropTypes from 'prop-types';
 
 import NewApiContainer from './newApiContainer';
 
-import Section from '../../Layout/Section/Section';
-import Title from '../../Layout/Title/Title';
-
 const propTypes = {
     location: PropTypes.object.isRequired,
 };
 
-const EditPage = ({ location }) => (
-    <div>
-        <Section>
-            <Title>New API</Title>
-        </Section>
-        <Section>
-            <NewApiContainer location={location} />
-        </Section>
-    </div>
-);
+const EditPage = ({ location }) => <NewApiContainer location={location} />;
 
 EditPage.propTypes = propTypes;
 
