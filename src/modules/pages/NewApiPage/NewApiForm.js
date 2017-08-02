5603750a67e167122f01e03a2322baa19edd16ed
ivanzusko <ivan.zusko@gmail.com>
2017-08-02 16:15:16 +0200

    fix top part section alignment

--- src/modules/pages/NewApiPage/NewApiForm.js
+++ src/modules/pages/NewApiPage/NewApiForm.js
@@ -39,38 +39,38 @@ const ApiForm = (props) => {
                         type="submit"
                         mod="primary"
                     >
-                        Submit
+                        Create API
                     </Button>
                 </Row>
-                <div className={b('inner')}>
-                    <FormRow>
-                        <FormInput component="input" label="Name" attachTo="name" type="text" tooltip="some another tooltip about something usefull" />
-                    </FormRow>
-                    <FormRow>
-                        <FormInput component="input" label="Listen Path" attachTo="proxy.listen_path" type="text" tooltip="LOREM IPSUM" />
-                        <FormInput component="input" label="Upstream URL" attachTo="proxy.upstream_url" type="text" />
-                        <FormInput component="input" label="Preserve HOST" attachTo="proxy.preserve_host" type="checkbox" normalize={v => !!v} />
-                        <FormInput component="input" label="Strip Path" attachTo="proxy.strip_path" type="checkbox" normalize={v => !!v} />
-                        <FormInput component="input" label="Append Path" attachTo="proxy.append_path" type="checkbox" normalize={v => !!v} />
-                    </FormRow>
-                    <Section>
-                        <FormRow>
-                            <FormLabel text="Health check" />
-                        </FormRow>
-                        <FormRow>
-                            <FormInput component="input" label="url" attachTo="health_check.url" type="text" />
-                            <FormInput component="input" label="timeout" attachTo="health_check.timeout" type="text" parse={parse} />
-                        </FormRow>
-                    </Section>
-
-
-                    {
-                        !!initialValues.plugins &&
-                            <RenderPlugins plugins={initialValues.plugins} />
-                    }
-
-                </div>
             </Section>
+            <div className={b('inner')}>
+                <FormRow>
+                    <FormInput component="input" label="Name" attachTo="name" type="text" tooltip="some another tooltip about something usefull" />
+                </FormRow>
+                <FormRow>
+                    <FormInput component="input" label="Listen Path" attachTo="proxy.listen_path" type="text" tooltip="LOREM IPSUM" />
+                    <FormInput component="input" label="Upstream URL" attachTo="proxy.upstream_url" type="text" />
+                    <FormInput component="input" label="Preserve HOST" attachTo="proxy.preserve_host" type="checkbox" normalize={v => !!v} />
+                    <FormInput component="input" label="Strip Path" attachTo="proxy.strip_path" type="checkbox" normalize={v => !!v} />
+                    <FormInput component="input" label="Append Path" attachTo="proxy.append_path" type="checkbox" normalize={v => !!v} />
+                </FormRow>
+                <Section>
+                    <FormRow>
+                        <FormLabel text="Health check" />
+                    </FormRow>
+                    <FormRow>
+                        <FormInput component="input" label="url" attachTo="health_check.url" type="text" />
+                        <FormInput component="input" label="timeout" attachTo="health_check.timeout" type="text" parse={parse} />
+                    </FormRow>
+                </Section>
+
+
+                {
+                    !!initialValues.plugins &&
+                        <RenderPlugins plugins={initialValues.plugins} />
+                }
+
+            </div>
         </form>
     );
 };
