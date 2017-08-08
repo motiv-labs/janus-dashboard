d47931953b0bfcde5846d694a53f0150e53aff60
ivanzusko <ivan.zusko@gmail.com>
2017-08-08 18:54:58 +0200

    remove some redundant code

--- src/modules/selects/TagSelect/TagSelect.js
+++ src/modules/selects/TagSelect/TagSelect.js
@@ -8,23 +8,15 @@ class TagSelect extends Component {
         multi: true,
         multiValue: [],
         options: this.props.options,
-        value: undefined,
     }
 
     handleOnChange = value => {
-        const { multi } = this.state;
-        console.error('VAL::: ', value);
-        console.log(this.props);
-
-        if (multi) {
-            this.setState((prevState, props) => ({ multiValue: value }), this.props.input.onChange(value.split(',')));
-        } else {
-            this.setState((prevState, props) => ({ value }), this.props.input.onChange(this.state.value.split(',')));
-        }
+        this.setState((prevState, props) => ({ multiValue: value }));
+        this.props.input.onChange(value.split(','));
     }
 
     render() {
-        const { multi, multiValue, options, value } = this.state;
+        const { multi, multiValue, options } = this.state;
         return (
             <Creatable
                 {...this.props}
@@ -34,7 +26,7 @@ class TagSelect extends Component {
                 simpleValue
                 options={this.state.options}
                 onChange={this.handleOnChange}
-                value={multi ? multiValue : value}
+                value={multiValue}
             />
         );
     }
