1e33ef6ecd99a4d46eb59f729b9cd2d8202e1a82
ivanzusko <ivan.zusko@gmail.com>
2017-08-14 18:34:53 +0200

    remove redundant stuff

--- src/modules/forms/plugins/RequestTransformer/HeadersSection/HeadersSection.js
+++ src/modules/forms/plugins/RequestTransformer/HeadersSection/HeadersSection.js
@@ -26,76 +26,43 @@ class HeadersSection extends Component {
         this.setState({config: this.props.config});
     }
 
-    handleAddHeader = () => {
-        console.log('ADD HEADER');
-        console.warn(this.state.config);
-        this.setState(prevState => {
-            console.log('prevState.config: ', prevState.config);
-            return {
-                config: prevState.config.concat({key: '', value: ''}),
-            };
-        });
-    }
     renderMembers = ({ fields }) => (
-    <ul>
-        <li>
-        <button type="button" onClick={() => fields.push({})}>Add Member</button>
-        </li>
-        {
-            fields.map((member, index) => {
-                console.warn('s/_/_/_/_/_/_/_/_/_/_/_/_/');
-                console.error('MEMBER:: ', member);
-                console.warn('e/_/_/_/_/_/_/_/_/_/_/_/_/');
-                return <li key={index}>
-                    <button
-                        type="button"
-                        title="Remove Member"
-                        onClick={() => fields.remove(index)}
-                    />
-                    <h4>Member #{index + 1}</h4>
+        <ul>
+            <li>
+            <button type="button" onClick={() => fields.push({})}>Add Member</button>
+            </li>
+            {
+                fields.map((member, index) => {
+                    console.warn('s/_/_/_/_/_/_/_/_/_/_/_/_/');
+                    console.error('MEMBER:: ', member);
+                    console.warn('e/_/_/_/_/_/_/_/_/_/_/_/_/');
+                    return <li key={index}>
+                        <button
+                            type="button"
+                            title="Remove Member"
+                            onClick={() => fields.remove(index)}
+                        />
+                        <h4>Member #{index + 1}</h4>
 
-                    <Field
-                        name={`${member}.key`}
-                        type="text"
-                        component={Input}
-                        label="First Name"
-                    />
-                    <Field
-                        name={`${member}.value`}
-                        type="text"
-                        component={Input}
-                        label="Last Name"
-                    />
-                </li>;
-            })
-        }
-    </ul>
+                        <Field
+                            name={`${member}.key`}
+                            type="text"
+                            component={Input}
+                            label="First Name"
+                        />
+                        <Field
+                            name={`${member}.value`}
+                            type="text"
+                            component={Input}
+                            label="Last Name"
+                        />
+                    </li>;
+                })
+            }
+        </ul>
     )
 
-    handleRemoveHeader = (i) => {
-        console.clear();
-        console.log(i);
-        // const newConfig = this.state.config.filter(item => item.key !== i);
-        R.remove(i, 1, this.props.config);
-        const newConfig = R.remove(i, 1, this.state.config);
-        console.error('NEW!!!!!!!!!! ', newConfig);
-        // this.setState(prevState => {
-        //     // console.log('prevState.config: ', i, prevState.config);
-        //     return {
-        //         config: newConfig,
-        //     };
-        // });
-        this.setState({ config: newConfig });
-        // console.error(this.state);
-    }
-
-    getSt = () => {
-        console.error(this.state.config);
-    }
-
     renderHeaders = ({ fields }) => {
-        // console.error('CONFIG ==> ', config);
-        console.error('FIELDS::::', fields);
         return (
             <div>
                 <li>
@@ -137,41 +104,7 @@ class HeadersSection extends Component {
         );
     }
 
-/*
-    renderHeaders = config => {
-        console.error('CONFIG ==> ', config);
-        return config.map((item, key) => {
-            console.warn(`${this.props.name}['${key}']`);
-
-            return (
-                <Row key={`${key}-${item.key}`} dataKey={item.key}>
-                    <Row col>
-                        <Field
-                            name={`${this.props.name}['${key}'].key`}
-                            type="text"
-                            placeholder="Key"
-                            component={Input}
-                        />
-                    </Row>
-                    <Row col>
-                        <Field
-                            name={`${this.props.name}['${key}'].value`}
-                            type="text"
-                            placeholder="Value"
-                            component={Input}
-                        />
-                    </Row>
-                    <button type="button" onClick={() => config.remove(key)}>remove</button><br />
-                </Row>
-            );
-        });
-    }
-*/
     render() {
-        console.error('THIS>PROPS>', this.props);
-        console.error('THIS>STATE>', this.state);
-        // const { config } = this.props;
-        // console.error(Object.keys(config));
         return (
             <Row col>
                 <Row>
