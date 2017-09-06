15b12663f4d55329872d6e1d71d7e1f352b8744f
ivanzusko <ivan.zusko@gmail.com>
2017-09-06 12:38:56 +0200

    add actual staging url

--- src/store/api/config.js
+++ src/store/api/config.js
@@ -1,5 +1,6 @@
 export default {
     gateway: {
-        uri: 'http://localhost:8081',
+        // uri: 'http://localhost:8081',
+        uri: 'http://ops-gateway002.staging.hellofresh.io:8081/',
     },
 };
