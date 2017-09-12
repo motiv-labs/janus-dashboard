6157a70e965f37a71f75ae41f0d346ba5fb3141b
ivanzusko <ivan.zusko@gmail.com>
2017-09-12 14:27:30 +0200

    --amend

--- src/modules/pages/LoginPage/loginContainer.js
+++ src/modules/pages/LoginPage/loginContainer.js
@@ -1,19 +1,17 @@
 import { connect } from 'react-redux';
 
 import {
-    checkLoggedStatus,
     authorizeThroughGithub,
 } from '../../../store/actions';
 
 import FormWrapper from './LoginForm';
 
 const mapStateToProps = state => ({
-    api: state.userSessionReducer.api,
     errorMsg: state.userSessionReducer.errorMsg,
     isFetching: state.requestReducer.isFetching,
 });
 
 export default connect(
     mapStateToProps,
-    { checkLoggedStatus, authorizeThroughGithub },
+    { authorizeThroughGithub },
 )(FormWrapper);
