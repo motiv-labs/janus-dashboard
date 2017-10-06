bc9d2eee6ce336b69ed7256abfbc1f737bc0ec36
ivanzusko <ivan.zusko@gmail.com>
2017-10-06 17:08:47 +0200

    --amend

--- src/store/reducers/healthcheck.reducer.js
+++ src/store/reducers/healthcheck.reducer.js
@@ -49,7 +49,6 @@ export default function reducer(state = initialState, action) {
                 ...state,
                 status: checkStatus(action.payload.status),
                 statusName: action.payload.status,
-                // statusText: action.payload.text,
                 healthcheckList: convertToList(action.payload.failures),
                 isFetching: false,
             };
