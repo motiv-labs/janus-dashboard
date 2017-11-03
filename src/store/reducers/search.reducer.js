e6fb05f2cf154437e26cf90d78e0054356499fe8
Sanjito Kurniawan <ssi@hellofresh.com>
2017-11-03 11:57:45 +0100

    Reset searchQuery when fetching endpoint/oath list pages

--- src/store/reducers/search.reducer.js
+++ src/store/reducers/search.reducer.js
@@ -1,5 +1,7 @@
 import {
     SEARCH_QUERY,
+    FETCH_ENDPOINTS_START,
+    FETCH_OAUTH_SERVERS_LIST_START,
 } from '../constants';
 
 export const initialState = {
@@ -8,6 +10,12 @@ export const initialState = {
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
+        case FETCH_ENDPOINTS_START:
+        case FETCH_OAUTH_SERVERS_LIST_START:
+            return {
+                ...state,
+                searchQuery: initialState.searchQuery
+            };
         case SEARCH_QUERY:
             return {
                 ...state,
