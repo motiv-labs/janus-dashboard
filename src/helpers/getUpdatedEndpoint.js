d6b48687db83e38d9698ace0cd6048e26b146394
ivanzusko <ivan.zusko@gmail.com>
2017-11-03 16:33:31 +0100

    move transpoframtions to helper function to make it reusable

---
+++ src/helpers/getUpdatedEndpoint.js
@@ -0,0 +1,23 @@
+import R from 'ramda';
+import transformFormValues from './transformFormValues';
+
+const getUpdatedEndpoint = values => selectedPlugins => {
+    const getTransformedValues = values => transformFormValues(values, true);
+    const extractPlugins = obj => obj.plugins;
+    const getAddedPlugins = arr => arr.filter(plugin => {
+        return selectedPlugins.indexOf(plugin.name) !== -1;
+    });
+    const createUpdatedEndpoint = arr => ({
+        ...getTransformedValues(values),
+        plugins: arr,
+    });
+
+    return R.compose(
+        createUpdatedEndpoint,
+        getAddedPlugins,
+        extractPlugins,
+        getTransformedValues,
+    )(values);
+};
+
+export default getUpdatedEndpoint;
