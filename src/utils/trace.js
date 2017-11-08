2be625c43d66630cf1ed527e18ebd65a106b871f
ivanzusko <ivan.zusko@gmail.com>
2017-11-08 18:57:13 +0100

    add trace function

---
+++ src/utils/trace.js
@@ -0,0 +1,28 @@
+import R from 'ramda';
+import { openResponseModal } from '../store/actions';
+
+/**
+ * @name trace
+ * @desc helper function for development only.
+ *          It should be used like this:
+ * @example (from https://github.com/MostlyAdequate/mostly-adequate-guide/blob/master/ch5.md#debugging)
+ *
+ * import trace from '../utilz/trace';
+ *
+ * const dasherize = R.compose(
+ *      join('-'),
+ *      toLower,
+ *      trace('after split'),
+ *      split(' '),
+ *      replace(/\s{2,}/ig, ' ')
+ * );
+ *
+ * dasherize('The world is a vampire');
+ * // after split [ 'The', 'world', 'is', 'a', 'vampire' ]
+ */
+const trace = R.curry(function(tag, x) {
+    console.error('trace -> ', tag, x);
+    return x;
+});
+
+export default trace;
