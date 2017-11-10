3e709401487bd4ae254dc6512273134599eb42ab
Sanjito Kurniawan <ssi@hellofresh.com>
2017-11-10 16:24:12 +0100

    Add Modal tests

---
+++ src/__tests__/components/Modal/Modal.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-15';
+
+import Modal from '../../../components/Modal/Modal';
+
+configure({ adapter: new Adapter() });
+
+describe('Modal component', () => {
+    const render = props => mount(<Modal {...props} />);
+    const mockFunction = jest.fn();
+    const requiredProps = {
+        closeModal: mockFunction,
+        show: true,
+        message: 'Modal test message',
+    };
+    const runCommonTest = (wrapper, props) => {
+        const tree = toJSON(wrapper);
+        const modaliz = tree.children[0];
+
+        expect(tree.props.show).toEqual(props.show);
+        expect(
+            wrapper.find('.j-confirmation__text').text()
+        ).toEqual(props.message);
+
+        modaliz.props.onClose();
+        expect(mockFunction).toHaveBeenCalled();
+        expect(tree).toMatchSnapshot();
+    };
+
+    beforeEach(() => {
+        mockFunction.mockReset();
+    });
+
+    it('renders correctly', () => {
+        const props = requiredProps;
+        const wrapper = render(props);
+        runCommonTest(wrapper, props);
+    });
+
+    it('renders correctly with title if an additional prop `title` is passed', () => {
+        const props = {
+            ...requiredProps,
+            title: 'Modal test title',
+        };
+        const wrapper = render(props);
+
+        runCommonTest(wrapper, props);
+        expect(
+            wrapper.find('.j-confirmation__title').text()
+        ).toEqual(props.title);
+    });
+
+    it('renders correctly with buttons if an additional prop `buttons` is passed', () => {
+        const props = {
+            ...requiredProps,
+            buttons: [
+                <button key={1}>Cancel</button>,
+                <button key={2}>Confirm</button>,
+            ],
+        };
+        const wrapper = render(props);
+
+        runCommonTest(wrapper, props);
+        expect(
+            wrapper.find('.j-confirmation__buttons-group').containsAllMatchingElements(props.buttons)
+        ).toBe(true);
+    });
+});
