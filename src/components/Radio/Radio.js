2066fdf4605c2923edf0d56c117ad110225c8c91
ivanzusko <ivan.zusko@gmail.com>
2017-11-15 16:16:49 +0100

    classNames to string

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import block from '../../../helpers/bem-cn';
+import block from '../../helpers/bem-cn';
 
 import './Radio.css';
 
@@ -10,21 +10,21 @@ const Radio = props => {
     const { input, id, name, type } = props;
 
     return (
-        <div className={radio('wrap')}>
+        <div className={radio('wrap')()}>
             <input
-                className={radio('input')}
+                className={radio('input')()}
                 type={type}
                 name={name}
                 id={id}
                 {...input}
             />
-            <div className={radio('d1')}>
-                <div className={radio('d2')}>
+            <div className={radio('d1')()}>
+                <div className={radio('d2')()}>
                     <div>
-                        <svg className={radio('svg', { '1': true })} viewBox="0 0 24 24">
+                        <svg className={radio('svg', { '1': true })()} viewBox="0 0 24 24">
                             <path className={radio('path')} d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
                         </svg>
-                        <svg className={radio('svg', { '2': true })}viewBox="0 0 24 24" >
+                        <svg className={radio('svg', { '2': true })()}viewBox="0 0 24 24" >
                             <path className={radio('path')} d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
                         </svg>
                     </div>
