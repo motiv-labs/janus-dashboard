8d0aa8cd18d8b975a97415069a5c2df65b92416b
Sanjito Kurniawan <ssi@hellofresh.com>
2017-11-17 11:50:58 +0100

    Redirect to main page on /login if already logged in

--- src/modules/pages/LoginPage/loginContainer.js
+++ src/modules/pages/LoginPage/loginContainer.js
@@ -9,6 +9,7 @@ import FormWrapper from './LoginForm';
 const mapStateToProps = state => ({
     errorMsg: state.userSessionReducer.errorMsg,
     isFetching: state.requestReducer.isFetching,
+    user: state.userSessionReducer.user,
 });
 
 export default connect(
