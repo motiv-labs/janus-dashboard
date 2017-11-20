0b9a3a355ea9e5a1a2a2f2d2c65c8cd093908d24
Sanjito Kurniawan <ssi@hellofresh.com>
2017-11-20 11:25:08 +0100

    OAuth save/update/delete should set isFetching to true

--- src/store/reducers/oAuthServer.reducer.js
+++ src/store/reducers/oAuthServer.reducer.js
@@ -3,6 +3,12 @@ import {
     FETCH_OAUTH_SERVER_SUCCESS,
     FETCH_OAUTH_SERVER_SCHEMA_START,
     FETCH_OAUTH_SERVER_SCHEMA_SUCCESS,
+    SAVE_OAUTH_SERVER_START,
+    SAVE_OAUTH_SERVER_SUCCESS,
+    UPDATE_OAUTH_SERVER_START,
+    UPDATE_OAUTH_SERVER_SUCCESS,
+    DELETE_OAUTH_SERVER_START,
+    DELETE_OAUTH_SERVER_SUCCESS,
 } from '../constants';
 
 export const initialState = {
@@ -13,11 +19,22 @@ export const initialState = {
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
+        case SAVE_OAUTH_SERVER_START:
+        case UPDATE_OAUTH_SERVER_START:
+        case DELETE_OAUTH_SERVER_START:
         case FETCH_OAUTH_SERVER_START:
         case FETCH_OAUTH_SERVER_SCHEMA_START: {
             return {
                 ...state,
                 isFetching: true,
+            };
+        }
+        case SAVE_OAUTH_SERVER_SUCCESS:
+        case UPDATE_OAUTH_SERVER_SUCCESS:
+        case DELETE_OAUTH_SERVER_SUCCESS: {
+            return {
+                ...state,
+                isFetching: false,
             };
         }
         case FETCH_OAUTH_SERVER_SUCCESS: {
