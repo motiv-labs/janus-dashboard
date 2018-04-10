7bceeeca1aa3caf1294667d396361f9905208f70
ivanzusko <ivan.zusko@gmail.com>
2018-04-10 14:29:25 +0200

    Render redis options

--- src/modules/forms/plugins/RateLimit/RateLimitPlugin.js
+++ src/modules/forms/plugins/RateLimit/RateLimitPlugin.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Field } from 'redux-form'
 import R from 'ramda'
+import Select from 'react-select'
 
 import block from '../../../../helpers/bem-cn'
 
@@ -22,79 +23,129 @@ const propTypes = {
   handlePluginExclude: PropTypes.func.isRequired
 }
 
-const RateLimitPlugin = ({
-  apiSchema,
-  className,
-  name,
-  handlePluginExclude,
-  plugin,
-  pluginFromValues,
-  pluginName,
-  response
-}) => {
-  const b = block(className)
-  const getLabels = plugins => plugins.filter(pl => pl.name === plugin.name)[0].config.limit.labels
+class RateLimitPlugin extends PureComponent {
+  constructor (props) {
+    super(props)
 
-  const createOptions = (list1, list2) => {
-    const combinedListOfUnitsAndLabels = R.zip(list1, list2)
+    this.state = {
+      selectedPolicy: {
+        name: this.props.pluginFromValues.config.policy.selected || ''
+      }
+    }
+  }
 
-    return combinedListOfUnitsAndLabels.map(item => ({
-      label: item[1],
-      value: item[0]
+  handleChangeSelectedPolicy = value => {
+    this.setState(() => ({
+      selectedPolicy: {
+        name: value.value
+      }
     }))
   }
 
-  return (
-    <div className={b('section')()}>
-      <Row fullwidth>
+  renderRedisOptions = value => {
+    if (value.name === 'redis') {
+      return <div>
         <Row col>
-          <Label>Plugin Name</Label>
-          <Input input={{value: 'Rate Limit'}} disabled />
-        </Row>
-        <ControlBar name={`${name}.enabled`} removePlugin={() => handlePluginExclude(pluginName)} />
-      </Row>
-      <Row className={b('row')()} fullwidth>
-        <Row col>
-          <div className={grid('row', { 2: true })}>
-            <Row col>
-              <Label>Limit Value</Label>
-              <Field
-                type='number'
-                name={`${name}.config.limit.value`}
-                placeholder=''
-                component={Input}
-              />
-            </Row>
-            <Row col>
-              <Label>Limit Unit</Label>
-              <Field
-                name={`${name}.config.limit.unit`}
-                type='text'
-                searchable={false}
-                clearable={false}
-                options={createOptions(plugin.config.limit.units, getLabels(apiSchema.plugins))}
-                component={SimpleSelect}
-              />
-            </Row>
-          </div>
-          <Hint>The maximum number of requests that the Gateway will forward to the upstream_path.</Hint>
-        </Row>
-        <Row col>
-          <Label>Policy</Label>
+          <Label>DSN</Label>
           <Field
-            name={`${name}.config.policy.selected`}
             type='text'
-            searchable={false}
-            clearable={false}
-            options={plugin.config.policy.options}
-            component={SimpleSelect}
+            name={`${this.props.name}.config.redis.dsn`}
+            placeholder=''
+            component={Input}
           />
-          <Hint>The type of rate-limiting policy used for retrieving and incrementing the limits.</Hint>
-          <Hint>The rate-limiting policies to use for retrieving and incrementing the limits. Available values are local (counters will be stored locally in-memory on the node) and redis (counters are stored on a Redis server and will be shared across the nodes).</Hint>
         </Row>
-      </Row>
-    </div>
-  )
+        <Row col>
+          <Label>Prefix</Label>
+          <Field
+            type='text'
+            name={`${this.props.name}.config.redis.prefix`}
+            placeholder=''
+            component={Input}
+          />
+        </Row>
+      </div>
+    }
+
+    return null
+  }
+
+  render () {
+    const {
+      apiSchema,
+      className,
+      name,
+      handlePluginExclude,
+      plugin,
+      pluginName
+    } = this.props
+
+    const b = block(className)
+    const getLabels = plugins => plugins.filter(pl => pl.name === plugin.name)[0].config.limit.labels
+
+    const createOptions = (list1, list2) => {
+      const combinedListOfUnitsAndLabels = R.zip(list1, list2)
+
+      return combinedListOfUnitsAndLabels.map(item => ({
+        label: item[1],
+        value: item[0]
+      }))
+    }
+
+    return (
+      <div className={b('section')()}>
+        <Row fullwidth>
+          <Row col>
+            <Label>Plugin Name</Label>
+            <Input input={{value: 'Rate Limit'}} disabled />
+          </Row>
+          <ControlBar name={`${name}.enabled`} removePlugin={() => handlePluginExclude(pluginName)} />
+        </Row>
+        <Row className={b('row')()} fullwidth>
+          <Row col>
+            <div className={grid('row', { 2: true })}>
+              <Row col>
+                <Label>Limit Value</Label>
+                <Field
+                  type='number'
+                  name={`${name}.config.limit.value`}
+                  placeholder=''
+                  component={Input}
+                />
+              </Row>
+              <Row col>
+                <Label>Limit Unit</Label>
+                <Field
+                  name={`${name}.config.limit.unit`}
+                  type='text'
+                  searchable={false}
+                  clearable={false}
+                  options={createOptions(plugin.config.limit.units, getLabels(apiSchema.plugins))}
+                  component={SimpleSelect}
+                />
+              </Row>
+            </div>
+            <Hint>The maximum number of requests that the Gateway will forward to the upstream_path.</Hint>
+          </Row>
+          <Row col>
+            <Label>Policy</Label>
+            <Select
+              className='j-select'
+              name={`${name}.config.policy.selected`}
+              type='text'
+              searchable={false}
+              clearable={false}
+              options={plugin.config.policy.options}
+              onChange={this.handleChangeSelectedPolicy}
+              value={this.state.selectedPolicy.name}
+            />
+            <Hint>The type of rate-limiting policy used for retrieving and incrementing the limits.</Hint>
+            <Hint>The rate-limiting policies to use for retrieving and incrementing the limits. Available values are local (counters will be stored locally in-memory on the node) and redis (counters are stored on a Redis server and will be shared across the nodes).</Hint>
+            { this.renderRedisOptions(this.state.selectedPolicy) }
+          </Row>
+        </Row>
+      </div>
+    )
+  }
 }
 
 RateLimitPlugin.propTypes = propTypes
