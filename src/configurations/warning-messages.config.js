9e43d56f43332673381f54c506a2c5206977bf87
Sanjito Kurniawan <ssi@hellofresh.com>
2019-02-01 10:39:20 +0100

    Update regexp pattern used for API Name validation

--- src/configurations/warning-messages.config.js
+++ src/configurations/warning-messages.config.js
@@ -1,5 +1,5 @@
 export default {
   LISTEN_PATH: 'Listen path should start from "/".',
-  NAMES: 'The name should contain only letters, "-" and/or "_".',
+  NAMES: 'The name should contain only alphanumeric letters and "-"',
   URL: 'Should be a valid url format("http://..." or "https://...").'
 }
